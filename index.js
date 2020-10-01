2a6c7b8965b32b120fd8d91227b1a2b2cf2ecd77
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-10-01 19:55:14 +0300

    Added all routes, configs and fixed templates

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const config = require('./config/config')[env];
 const app = require('express')();
 
 require('./config/express')(app);
-require('./config/routes')(app);
+require('./routes')(app);
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(
+	config.port,
+	console.log(`Listening on port ${config.port}! Now its up to you...`)
+);
